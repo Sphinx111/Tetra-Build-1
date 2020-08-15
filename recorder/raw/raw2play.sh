2b9aef7d8b1599f28e9053d89ebc4e2e693b03e4
larryth <dev@logami.fr>
2020-08-15 10:06:05 +0200

    @orestescaminha updated its script. Now wav files are stored in wav folder.

--- recorder/raw/raw2play.sh
+++ recorder/raw/raw2play.sh
@@ -2,43 +2,40 @@
 # 2020-08-13 Original source code from @orestescaminha
 # 2020-08-13 Additional notes by Larry "chmod u+rwx raw2play.sh" should be enough, no root access required
 #
-# Save this in recorder/wav dir, "chmod 777 raw2play.sh" and run as ./out2play.sh
-# Needs inotify-tools if it is not already installed
+# Save this in recorder/raw dir, "chmod 777 raw2play.sh" and run as ./raw2play.sh
+# Needs "apt-get install inotify-tools" if it is not already installed
 # Run recorder using -a flag
 #
-# get the current path and raw path
+# get the current path (recorder/raw) and wav path (recorder/wav)
 CURPATH=`pwd`
-TARGET=../raw
+WAVPATH=../wav
 #echo $CURPATH
-#echo $TARGET
-while FILERAW=$(inotifywait -e create $TARGET --format %f) 
+#echo $WAVPATH
+while FILERAW=$(inotifywait -e create $CURPATH --format %f) 
 	do
 	while true
 	    do       #Tests whether the .raw file is finished
-		    A=$(stat -c %s $TARGET/$FILERAW)
+		    A=$(stat -c %s $CURPATH/$FILERAW)
 		    #echo $A Bytes is the size of $FILERAW
 	        sleep 2
-            B=$(stat -c %s $TARGET/$FILERAW)
+            B=$(stat -c %s $CURPATH/$FILERAW)
             #echo $B Bytes is is the size of $FILERAW after 2s
     	    DIFF=$((B-A))
             #echo $DIFF Bytes is the diference
 		    
 			if [ "$DIFF" == 0 ]; then
-                echo "====Speech completed===="
-				break  # Returns to the beginning of the loop if the conversation has not ended
+                echo "!!!!!!!!!!! .raw file completed !!!!!!!!!!!"
+				break  # Returns to the beginning of the loop if .raw file has not completed
 		    else
-			    echo "==== Waiting for speech completion ===="
+			    echo "==== Waiting for .raw file completion ===="
 			fi
 		done
 		
-	cp -rf $TARGET/$FILERAW $CURPATH
-        for FILE in $CURPATH/$FILERAW; do
-            BASE=$(echo "$FILE" | cut -f 1 -d '.');	 # piping cut to remove the extension .out
-            echo "$FILE -> $BASE.wav" >/dev/null 2>&1 &
-
-			sox -r 8k -e signed -b 16 "$BASE.raw" "$BASE.wav"
-			echo Listen to $BASE.wav
-			play "$BASE.wav" >/dev/null 2>&1 &
+        for FILE in $FILERAW; do
+            BASE=$(echo "$FILE" | cut -f 1 -d '.');	 # piping cut to remove the extension .raw
+			echo "$BASE -> $WAVPATH/$BASE.wav" >/dev/null 2>&1 &
+            sox -r 8k -e signed -b 16 "$BASE.raw" "$WAVPATH/$BASE.wav"
+			echo "Listening to $BASE.wav ($B Bytes)"
+			play $WAVPATH/$BASE.wav >/dev/null 2>&1 &
 	    done
-    rm  *.raw
     done
